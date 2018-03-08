0a34fad52d1d6e537ed0fc304d3096220197ade4
Wladimir Bandeira <tecnico100@gmail.com>
2018-03-08 17:39:33 -0300

    first commit

---
+++ mdw/src/main/java/br/com/mdw/MdwApplication.java
@@ -0,0 +1,12 @@
+package br.com.mdw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdwApplication.class, args);
+	}
+}
